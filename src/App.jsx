67f85224c7c07897fca8a8baffa6b1940aa05403
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2025-12-22 18:01:58 +0100

    Initial vite react setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { GeneralInfo } from "./components/GeneralInfo";
+
+function App() {
+  return (
+    <>
+      <h1>CV Application</h1>
+    </>
+  );
+}
+
+export default App;
